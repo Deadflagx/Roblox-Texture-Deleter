8910c09916e2ff204173a594b35ed93317198220
Deadflagx <68439718+Deadflagx@users.noreply.github.com>
2021-04-18 15:17:15 +0300

    success msg

--- source/main.py
+++ source/main.py
@@ -78,6 +78,7 @@ def move_textures_to_backup(path, path_to, settings):
 		
 
 if __name__ == '__main__':
+	#C:/Users/Deadflag/AppData/Local/Roblox/Versions/version-0137f7d4848f436a/PlatformContent/pc/textures
 	logging.basicConfig(level=logging.DEBUG, filename='error.log', format='%(asctime)s %(levelname)s:%(message)s', filemode="w")
 	logging.info("Applicationg Run")
 	current_username = os.getlogin()
@@ -88,6 +89,7 @@ if __name__ == '__main__':
 	create_backup_folder(path)
 	clear_backup(f'{path}/Backup Textures')
 	move_textures_to_backup(path, f'{path}/Backup Textures', settings)
+	show_msgbox( f'The textures were removed successfully. You can find the deleted folders along the path: {path}/Backup Textures', 'Success')
 	#main()
 
 
